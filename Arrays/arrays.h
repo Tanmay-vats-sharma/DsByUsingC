fa4326bcf2367664ce9e0e056f86d00a4ba08799
Tanmay Sharma <tanmaysharmats36@gmail.com>
2025-01-05 02:13:29 +0530

    insertAtEnd function made

--- Arrays/arrays.h
+++ Arrays/arrays.h
@@ -1,8 +1,19 @@
 #include<stdio.h>
-void display(int arr[],int n){
+
+void display(int a[],int *n){
+  printf("%d", *n);
   printf("Currently array looks like:\n");
-  for (int i = 0; i < n;i++)
+  for (int i = 0; i < *n;i++)
   {
-    printf("a[%d] = %d\n", i, arr[i]);
+    printf("a[%d] = %d\n", i, a[i]);
   }
+}
+
+void insertAtEnd(int a[],int *n){
+  int item;
+  printf("Enter the Element you want to insert: ");
+  scanf("%d", &item);
+  *n += 1;
+  a[(*n)-1] = item;
+  
 }
--- Arrays/main.c
+++ Arrays/main.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 #include"arrays.h"
-
+int a[50];
 void main(){
-  int i, n ,a[50];
+  int i, n ,choice, option;
   printf("Enter number of elements of array");
   scanf("%d", &n);
   printf("Enetr items of Array");
@@ -10,6 +10,43 @@ void main(){
     printf("a[%d] =", i);
     scanf("%d", &a[i]);
   }
-  printf("Choose your Operation to perform:\n");
+  do
+  {
+    printf("Choose your Operation to perform:\n");
+    printf("1. Display");
+    printf("2. Insertion");
+    printf("3. Deletion");
+    printf("4. Exit");
+    printf("Enetr your choice 1 or 2 or 3 or 4: ");
+    scanf("%d", &choice);
+    switch (choice)
+    {
+    case 1:
+      display(a, &n);
+      break;  
+    case 2:
+      printf("Types of insertion");
+      printf("1. Insertion at Ending");
+      printf("2. Insertion at Beggining");
+      printf("3. Insertion at Specific Position");
+      printf("Enter your Choice: ");
+      scanf("%d", &option);
+      switch (option)
+      {
+      case 1:
+        insertAtEnd(a,&n);
+        break;
+      
+      default:
+        break;
+      }
+      break;
+    
+    default:
+      break;
+    }
+  } while (choice!=4);
   
+  
+
 }
